aff6a74ab3e903aacd26ab520db3b74278bb55a3
Nathan Daly <nhdaly@gmail.com>
2018-07-25 14:06:47 -0400

    Fix timestamp in julia prompt to update correctly on new line

--- juliarc.nhdaly.jl
+++ juliarc.nhdaly.jl
@@ -1,32 +1,28 @@
-macro _replace_callback(callback, newval)
-  quote
-      tmp = $callback
-      $callback = (args...)->(tmp(args...), $newval(args...))
-  end
+# Set timestamp and version number in Prompt
+function prompt()
+    timestamp = Dates.format(now(), "HH:MM:SS")
+    workingdirectory = last(splitdir(pwd()))
+    return string("($timestamp) julia$(VERSION.major).$(VERSION.minor)> ")
 end
+function customizeprompts(repl)
+    if !isdefined(repl, :interface)
+        repl.interface = Base.REPL.setup_interface(repl)
+    end
+    # prompt prefix function just sets the prompt
+    prefix = repl.interface.modes[1].prompt_prefix
+    repl.interface.modes[1].prompt_prefix = function ()
+        repl.interface.modes[1].prompt = prompt()
+        return (isa(prefix, Function) ? prefix() : prefix)
+    end
+end
+atreplinit(customizeprompts)
 
 try
   import OhMyREPL
   OhMyREPL.output_prompt!(">>")
   OhMyREPL.BracketInserter.enable_autocomplete_brackets(false)
-
-  # Set version number in prompt
-  in_prompt = "julia$(Int(VERSION.major)).$(Int(VERSION.minor))>"
-  OhMyREPL.input_prompt!(in_prompt)
-
-  # Set time in prompt:
-  set_time_in_prompt(_...) = OhMyREPL.input_prompt!("($(Dates.format(Dates.now(), "HH:MM:SS"))) $in_prompt")
-  set_time_in_prompt()
-  #   Update on keypress
-  OhMyREPL.add_pass!(OhMyREPL.PASS_HANDLER, "DisplayTime", set_time_in_prompt, true)
-
-  #   Update on Command finished
-  #main_mode = Base.active_repl.interface.modes[1]
-  #@_replace_callback(main_mode.on_done, set_time_in_prompt)
-  #@_replace_callback(main_mode.on_enter, set_time_in_prompt)
-
-
 catch end
+
 try
   using Revise
 catch
