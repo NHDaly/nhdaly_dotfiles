66f75093a7559576f6a4d4906cf347b1d556447a
Nathan Daly <nhdaly@gmail.com>
2018-09-07 13:36:15 -0400

    Simplified juilarc

--- juliarc.nhdaly.jl
+++ juliarc.nhdaly.jl
@@ -1,31 +1,3 @@
-if VERSION >= v"0.7-"
-  try
-    using REPL, Dates
-  catch e
-    println("WARNING: $e")
-  end
-else
-  REPL = Base.REPL
-end
-
-## Set timestamp and version number in Prompt
-#function prompt()
-#    timestamp = Dates.format(now(), "HH:MM:SS")
-#    return string("($timestamp) julia$(VERSION.major).$(VERSION.minor)> ")
-#end
-#function customizeprompts(repl)
-#    if !isdefined(repl, :interface)
-#        repl.interface = REPL.setup_interface(repl)
-#    end
-#    # prompt prefix function just sets the prompt
-#    prefix = repl.interface.modes[1].prompt_prefix
-#    repl.interface.modes[1].prompt_prefix = function ()
-#        repl.interface.modes[1].prompt = prompt()
-#        return (isa(prefix, Function) ? prefix() : prefix)
-#    end
-#end
-#atreplinit(customizeprompts)
-
 try
   import OhMyREPL
   OhMyREPL.input_prompt!(string("julia$(VERSION.major).$(VERSION.minor)>"))
