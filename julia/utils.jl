5fb2a8fd68f27afad08d259e98162f97a31ac28f
Nathan Daly <nhdaly@gmail.com>
2019-08-05 16:08:32 -0400

    add diff_fields; fix func_all_backedges

--- julia/utils.jl
+++ julia/utils.jl
@@ -10,17 +10,44 @@ macro dumpexpr(e)
     :(dump($(QuoteNode(e))))
 end
 
+"""
+    func_all_specializations(f)
+
+List all the specializations of a function, if there are any.
+"""
 func_all_specializations(f) =
     Dict(s.sig => s
      for m in methods(f).ms
      for s in Channel() do ch; s = m.specializations; while s isa Core.TypeMapEntry; put!(ch, s) ; s=s.next end end)
 
+"""
+    func_all_backedges(f)
+
+List all the backedges on all specializations of a function, if there are any.
+"""
 func_all_backedges(f) =
-    union(
-        Dict(:MethodTable => try methods(f).mt.backedges catch ; [] end),
-        Dict(k => try s.func.backedges catch ; [] end
-            for (k,s) in func_all_specializations(f)),
-    )
+        Dict(
+            :MethodTable => try methods(f).mt.backedges catch ; [] end,
+            (k => try s.func.backedges catch ; [] end
+             for (k,s) in func_all_specializations(f))...
+        )
+
+"""
+    diff_fields(a,b)
+
+Returns a Dict(:field => (a.field, b.field)) for each field present in both a and b that
+have different values.
+"""
+diff_fields(a,b) =
+    Dict(field => (af, bf)
+            for field in propertynames(a)
+            for af in (trygetfield(a, field),)
+            for bf in (trygetfield(b, field),)
+            if field in propertynames(b) && af != bf
+        )
+struct NotExists end
+const notexists = NotExists()
+trygetfield(o, f) = try getfield(o, f) catch; notexists end
 
 end
 
