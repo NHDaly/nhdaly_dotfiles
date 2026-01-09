3ea1dba936aa20e896cf09b86113afc0aa3f74ea
Nathan Daly <nathan.daly@relational.ai>
2026-01-09 12:45:21 -0700

    update vim / vscode for copilot and vim settings

--- juliarc.nhdaly.jl
+++ juliarc.nhdaly.jl
@@ -1,4 +1,7 @@
-using Revise
+isinteractive() && using Revise
 include(homedir()*"/dotfiles/nhdaly/julia/utils.jl")
+
+#get!(ENV, "RETESTITEMS_NWORKERS", 2)
+get!(ENV, "RETESTITEMS_NWORKER_THREADS", "1,1")
 
 
